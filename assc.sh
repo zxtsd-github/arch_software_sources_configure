eac952226cc77894c134b5c96689231ad1adb326
zxtsd <56102825+zxtsd-github@users.noreply.github.com>
2024-07-01 22:37:26 +0800

    Update assc.sh

--- assc.sh
+++ assc.sh
@@ -2,6 +2,7 @@
 sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 sudo cp ./mirrorlist /etc/pacman.d
 sudo pacman -Syyu
+sudo pacman -S archlinux-keyring
 
 sudo cat ./archlinuxcn >> /etc/pacman.conf
 sudo pacman-key --lsign-key "farseerfc@archlinux.org"
